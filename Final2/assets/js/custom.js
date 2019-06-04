cf3548cf0a9339b175bc198ac5162ebf2332a343
XeniaRuchka <censored.cheese@gmail.com>
2019-06-03 19:41:10 -0800

    Final changes on project
    Making new pages and layouts

---
+++ Final2/assets/js/custom.js
@@ -0,0 +1,33 @@
+﻿
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+       
+        reviews_fun:function()
+        {
+            ($)(function () {
+                $('#carousel-example').carousel({
+                    interval: 3000 
+                });
+            });
+
+        },
+     
+        
+        
+        
+
+        },
+
+    }
+   
+   
+    $(document).ready(function () {
+        mainApp.reviews_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
+
+
